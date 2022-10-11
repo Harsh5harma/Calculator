aaaf7661c00d3e1416e63338b10d0343e43a01d1
Harsh <harshsharma41664@gmail.com>
2022-10-12 05:04:22 +0530

    wrote a really basic dom script

--- script.js
+++ script.js
@@ -23,3 +23,38 @@ function operate(a,op,b){
     }
 }
 
+const numbers = Array.from(document.querySelectorAll(".number"));
+
+
+let clickables=[];
+for (let i=0;i<numbers.length;++i){
+    clickables.push(numbers[i]);
+}
+clickables.push(document.querySelector('.d'));
+clickables.push(document.querySelector('.clear'));
+clickables.push(document.querySelector('.equals'));
+
+console.table(clickables);
+clickables.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        let content;
+        let display = document.querySelector(".display");
+        console.log(button);
+        if (button.className==="clear"){
+            display.textContent='';
+        }else if(button.className==="d"){
+            content=display.textContent;
+            content=content.slice(0,content.length-1);
+            display.textContent=content;
+        }
+
+        if (button.dataset.num){
+            content = button.dataset.num;
+            display.textContent=display.textContent+content;
+        }else if (button.dataset.opcode){
+            content=button.dataset.opcode;
+            display.textContent=display.textContent+content;
+        }
+        
+    });
+})
